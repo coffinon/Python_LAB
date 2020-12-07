ce807f340fab4bebd2085a4630253571dbce90f9
Kacper Kupiszewski <k.kupiszewski4@gmail.com>
2020-12-07 14:03:54 +0100

    input / output - completed

---
+++ Wejscie_wyjscie/wprowadzanie_danych.py
@@ -0,0 +1,5 @@
+name, surname, birth_date = input("Podaj imie, nazwisko i date urodzenia po spacji: ").split(" ")
+
+print("Imie : ", name)
+print("Nazwisko : ", surname)
+print("Data urodzenia : ", birth_date)
---
+++ Wejscie_wyjscie/zapis_danych.py
@@ -0,0 +1,8 @@
+lock_code = "1234"
+
+input_code = input("Podaj kod : ")
+
+if input_code == lock_code:
+    print("Kod sie zgadza")
+else :
+    print("Kod sie nie zgadza")
